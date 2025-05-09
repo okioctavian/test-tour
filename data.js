5d07d15b42583156a8ae377ee36031b143a6fd65
okioctavian <okioctaviann@gmail.com>
2025-05-09 12:45:50 +0700

    tour

---
+++ data.js
@@ -0,0 +1,168 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-halaman-depan",
+      "name": "halaman depan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.397892632121691
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.004027682889217843,
+          "pitch": 0.09233108534489887,
+          "rotation": 0,
+          "target": "1-lorong"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Ruangan",
+          "text": "Balkon kosan"
+        }
+      ]
+    },
+    {
+      "id": "1-lorong",
+      "name": "lorong",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.397892632121691
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0012717813954203905,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-depan-kamar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Ruangan",
+          "text": "Lorong kosan"
+        }
+      ]
+    },
+    {
+      "id": "2-depan-kamar",
+      "name": "depan kamar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.397892632121691
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.005370243852290457,
+          "pitch": 0.21234499739845347,
+          "rotation": 0,
+          "target": "3-dalm-kosan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Ruangan",
+          "text": "Depan kamar doyok"
+        }
+      ]
+    },
+    {
+      "id": "3-dalm-kosan",
+      "name": "dalm kosan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.2957356079810438,
+        "pitch": 0.16785589720713645,
+        "fov": 1.397892632121691
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.2957356079810438,
+          "pitch": 0.16785589720713645,
+          "title": "Ruangan",
+          "text": "Kamar Doyok"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
